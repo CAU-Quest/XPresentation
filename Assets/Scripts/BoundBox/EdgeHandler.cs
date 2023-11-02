c789f21c3c4354154dff50cf5fe2c66bec178387
Frustman <youhk40@naver.com>
2023-11-03 03:37:40 +0900

    Fixes : Bound Box

--- Assets/Scripts/BoundBox/EdgeHandler.cs
+++ Assets/Scripts/BoundBox/EdgeHandler.cs
@@ -5,28 +5,44 @@ using UnityEngine;
 public class EdgeHandler : MonoBehaviour
 {
     public GameObject cursor;
-    
+
     public Vector3 initialLocalPosition;
     public Vector3 initialRotation;
 
+    public Quaternion beforeRotation;  // 이전 회전 상태를 저장하기 위한 변수
+    public Quaternion afterRotation;   // 누적 회전값을 저장하기 위한 변수
+
     public bool isSelected = false;
+
+    // 선택한 축을 나타내는 열거형
+    public enum RotationAxis
+    {
+        X,
+        Y,
+        Z
+    }
+
+    public RotationAxis selectedAxis = RotationAxis.X; // 기본적으로 X 축을 선택
+
     private void Start()
     {
         cursor = XRSelector.Instance.Cursor;
         initialLocalPosition = transform.localPosition;
         initialRotation = (transform.position - XRSelector.Instance.transform.position).normalized;
+        beforeRotation = XRSelector.Instance.selectedObject.transform.rotation;
+        afterRotation = Quaternion.identity;
     }
 
     public void Select()
     {
+        beforeRotation = XRSelector.Instance.selectedObject.transform.rotation;
         cursor = XRSelector.Instance.Cursor;
         cursor.transform.position = transform.position;
         initialLocalPosition = transform.localPosition;
         initialRotation = (cursor.transform.position - XRSelector.Instance.transform.position).normalized;
         isSelected = !isSelected;
     }
-    
-    
+
     private void Update()
     {
         if (isSelected)
@@ -37,8 +53,26 @@ public class EdgeHandler : MonoBehaviour
             // 회전값 계산
             Quaternion rotation = Quaternion.FromToRotation(initialRotation, directionToCenter);
 
-            XRSelector.Instance.selectedObject.transform.rotation = rotation;
-            XRSelector.Instance.transform.rotation = rotation;
+            afterRotation = rotation * beforeRotation;  // 누적 회전값 업데이트
+
+            if (selectedAxis == RotationAxis.X)
+            {
+                // X 축 회전만 적용
+                XRSelector.Instance.selectedObject.transform.rotation = Quaternion.Euler(afterRotation.eulerAngles.x, beforeRotation.eulerAngles.y, beforeRotation.eulerAngles.z);
+                XRSelector.Instance.transform.rotation = Quaternion.Euler(afterRotation.eulerAngles.x, beforeRotation.eulerAngles.y, beforeRotation.eulerAngles.z);
+            }
+            else if (selectedAxis == RotationAxis.Y)
+            {
+                // Y 축 회전만 적용
+                XRSelector.Instance.selectedObject.transform.rotation = Quaternion.Euler(beforeRotation.eulerAngles.x, afterRotation.eulerAngles.y, beforeRotation.eulerAngles.z);
+                XRSelector.Instance.transform.rotation = Quaternion.Euler(beforeRotation.eulerAngles.x, afterRotation.eulerAngles.y, beforeRotation.eulerAngles.z);
+            }
+            else if (selectedAxis == RotationAxis.Z)
+            {
+                // Z 축 회전만 적용
+                XRSelector.Instance.selectedObject.transform.rotation = Quaternion.Euler(beforeRotation.eulerAngles.x, beforeRotation.eulerAngles.y, afterRotation.eulerAngles.z);
+                XRSelector.Instance.transform.rotation = Quaternion.Euler(beforeRotation.eulerAngles.x, beforeRotation.eulerAngles.y, afterRotation.eulerAngles.z);
+            }
         }
     }
 }
