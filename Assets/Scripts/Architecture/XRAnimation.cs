64e148ea8828265740253a3f17ea7a076be6577e
cmne3602 <cmne3602@gmail.com>
2023-12-01 18:57:45 +0900

    Feat : Animation Preview Play
    has Bug

--- Assets/Scripts/Architecture/XRAnimation.cs
+++ Assets/Scripts/Architecture/XRAnimation.cs
@@ -14,14 +14,36 @@ public class XRAnimation : XRIAnimation
     public SlideObjectData previousData;
     public SlideObjectData nextData;
     public Ease ease = Ease.Linear;
-    
+    private Sequence _sequence;
+
+    private Transform _previousTransform;
+    private Material _previousMaterial;
+
+    public XRAnimation() 
+    {
+        _sequence = DOTween.Sequence().SetAutoKill(false)
+        .OnStart(() =>
+        {
+            _previousTransform.position = previousData.position;
+            _previousTransform.rotation = previousData.rotation;
+            _previousTransform.localScale = previousData.localScale;
+            _previousMaterial.color = previousData.color;
+        })
+        .Append(_previousTransform.DOMove(nextData.position, 1f).SetEase(ease))
+        .Join(_previousTransform.DORotate(nextData.rotation, 1f).SetEase(ease))
+        .Join(_previousTransform.DOLocalScale(nextData.localScale, 1f).SetEase(ease))
+        .Join(_previousMaterial.DOColor(nextData.color, 1f).SetEase(ease))
+        .AppendInterval(0.7f)
+        .SetLoops(-1, LoopType.Restart);
+    }
+
     public void Play()
     {
         float t = DOVirtual.EasedValue(0f, 1f, MainSystem.Instance.slideInterval, ease);
         Debug.Log("Playing t : " + t);
-        
+
         SlideObjectData lerpData = new SlideObjectData();
-        
+
         lerpData.position = Vector3.LerpUnclamped(previousData.position, nextData.position, t);
         lerpData.rotation = Quaternion.LerpUnclamped(previousData.rotation, nextData.rotation, t);
         lerpData.scale = Vector3.LerpUnclamped(previousData.scale, nextData.scale, t);
@@ -29,9 +51,26 @@ public class XRAnimation : XRIAnimation
         lerpData.isVisible = nextData.isVisible;
         lerpData.isGrabbable = nextData.isGrabbable;
         lerpData.isVideo = false;
-        
-        
+
+
         presentationObject.ApplyDataToObject(lerpData);
+    }
+
+    public void PlayPreview(Transform prevTrans, Material prevMat)
+    {
+        _previousTransform= prevTrans;
+        _previousMaterial= prevMat;
+
+        _sequence.Play();
+    }
+
+    public void StopPreview() 
+    {
+        _previousTransform.position = previousData.position;
+        _previousTransform.rotation = previousData.rotation;
+        _previousTransform.localScale = previousData.localScale;
+        _previousMaterial.color = previousData.color;
+        _sequence.Kill();
     }
 
     public void SetEase(Ease newEase)
